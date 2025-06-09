6a450e41e8a84477d572ce983eda0b76fb1e1dfe
Danial Kazemi <web.kazemi@gmail.com>
2025-06-09 19:17:53 +0330

    edit action register

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -5,7 +5,7 @@ function RegisterLayout ({children} : Readonly<{children:React.ReactNode}>) {
   return (
     <section className="register-page h-lvh grid grid-row-12 dark:bg-gray-900 text-black dark:text-white">
       <Header />
-      {children}      
+      {children}
       <Footer />
     </section>
   )
--- lib/actions/user.ts
+++ lib/actions/user.ts
@@ -1,15 +1,33 @@
 "use server"
+import db from "../db";
+import User from "@/app/models/users";
+
 const register = async (formData: FormData) => {
     try{
-
         const firstName = formData.get('firstName') as string;        
         const lastName = formData.get('lastName') as string;
         const email = formData.get('email') as string;
-        const password = formData.get('password') as string;
-        console.log(firstName,lastName, password, email);
-        
-    }catch{
+        const password = formData.get('password') as string;        
+        const data = {
+            firstName,
+            lastName,            
+            email,
+            password,
+            createdAt : new Date(),
+        }
 
+        await db.connect();
+        const newUser = new User(data);
+        await newUser.save();
+// const Tank = mongoose.model('Tank', yourSchema);
+// const small = new Tank({ size: 'small' });
+// await small.save();
+// // or
+// await Tank.create({ size: 'small' });
+// // or, for inserting large batches of documents
+// await Tank.insertMany([{ size: 'small' }]);    
+    }catch(error: any){
+        throw new Error(error);
     }
 }
 
